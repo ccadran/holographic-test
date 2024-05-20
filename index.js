6e31196bef45ad5aeb9110c55f92bfccc7721d10
ccadran <clariocadran33@gmail.com>
2024-05-20 15:29:40 +0200

    edit clip path

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const el = document.querySelector(".card");
-const wrap = document.querySelector(".card__wrapper");
 const card_3d = document.querySelector(".card__3d");
 let w = el.clientWidth;
 let h = el.clientHeight;
 let b = el.getBoundingClientRect();
+let lastX = 0; // Stocke la dernière position X de la souris
+let lastY = 0; // Stocke la dernière position Y de la souris
 
-const defaultBackgound = () => {
+const defaultBackground = () => {
   document.documentElement.style.setProperty("--x", "50%");
   document.documentElement.style.setProperty("--y", "50%");
   document.documentElement.style.setProperty("--bg-x", "50%");
@@ -13,11 +14,15 @@ const defaultBackgound = () => {
   document.documentElement.style.setProperty("--r-x", "0deg");
   document.documentElement.style.setProperty("--r-y", "0deg");
 };
-defaultBackgound();
+
+defaultBackground();
 
 el.addEventListener("mousemove", (e) => {
   let X = (e.clientX - b.left) / w;
   let Y = (e.clientY - b.top) / h;
+
+  lastX = X; // Met à jour la dernière position X de la souris
+  lastY = Y; // Met à jour la dernière position Y de la souris
 
   let rX = -(X - 0.5) * 26;
   let rY = (Y - 0.5) * 26;
@@ -27,14 +32,18 @@ el.addEventListener("mousemove", (e) => {
 
   document.documentElement.style.setProperty("--x", 100 * X + "%");
   document.documentElement.style.setProperty("--y", 100 * Y + "%");
-
   document.documentElement.style.setProperty("--bg-x", bgX + "%");
   document.documentElement.style.setProperty("--bg-y", bgY + "%");
-
   document.documentElement.style.setProperty("--r-x", rX + "deg");
   document.documentElement.style.setProperty("--r-y", rY + "deg");
 });
 
 el.addEventListener("mouseleave", () => {
-  defaultBackgound();
+  // Utilise les dernières valeurs de position de la souris à l'intérieur de la carte
+  document.documentElement.style.setProperty("--x", 100 * lastX + "%");
+  document.documentElement.style.setProperty("--y", 100 * lastY + "%");
+
+  // Réinitialise les valeurs de rotation
+  document.documentElement.style.setProperty("--r-x", "0deg");
+  document.documentElement.style.setProperty("--r-y", "0deg");
 });
